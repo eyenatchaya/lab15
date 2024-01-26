af3a805413949e8f4e94f13926dbf4c372de3364
eye <eyenathaya5@gmail.com>
2024-01-26 16:38:27 +0700

    Filled out lab15_4

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -17,3 +17,23 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &x,int &y,int &z,int &i){
+    int g[]={x,y,z,i};
+    int a = rand()%4;
+    int b = rand()%4;
+	int c = rand()%4;
+	int d = rand()%4;
+
+	while(a==b || c==d || a==c || a==d || b==c || b==d ){
+	 a = rand()%4;
+     b = rand()%4;
+	 c = rand()%4;
+	 d = rand()%4;
+	}
+
+	x=g[a];
+	y=g[b];
+	z=g[c];
+	i=g[d];
+}
